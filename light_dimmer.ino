f496138892af4bddc3dc22c8721ec3760107885b
arnaud leduc <arnaud.leduc@keyconsulting.fr>
2024-11-07 22:58:00 +0100

    use eeprom

--- light_dimmer.ino
+++ light_dimmer.ino
@@ -1,6 +1,8 @@
 #include "IRremote.hpp"
+#include <EEPROM.h>
 
 #define IR_RECEIVE_PIN 2
+
 #define DECREASE_K 12
 #define INCREASE_K 16
 #define FULL_WHITE 13
@@ -11,29 +13,39 @@
 #define ON 7
 #define DECREASE_P 4
 #define INCREASE_P 5
+#define WARM_ADDRESS 0
+#define COOL_ADDRESS 1
 
 // Définition des broches
-const int pinWarm = 6;  // PWM pour W- (blanc chaud)
-const int pinCool = 5;  // PWM pour C- (blanc froid)
-const int potPin = A0;  // Potentiomètre connecté à l'entrée analogique A0
-int potValue;
-int coolValue = 127;  // Intensité du blanc froid
-int warmValue = 127;  // Intensité du blanc chaud (inverse)
+const uint8_t pinWarm = 6;  // PWM pour W- (blanc chaud)
+const uint8_t pinCool = 5;  // PWM pour C- (blanc froid)
+uint8_t potValue;
+uint8_t coolValue = 127;  // Intensité du blanc froid
+uint8_t warmValue = 127;  // Intensité du blanc chaud (inverse)
 int power, ratio;
+bool hasChanged = false;
+unsigned long time;
 
 void setup() {
   Serial.begin(9600);
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   pinMode(pinWarm, OUTPUT);
   pinMode(pinCool, OUTPUT);
-  pinMode(potPin, INPUT);
-  analogWrite(3, 255);
-  analogWrite(5, 255);
-  analogWrite(6, 255);
+  coolValue = EEPROM.read(COOL_ADDRESS);
+  warmValue = EEPROM.read(WARM_ADDRESS);
+  analogWrite(pinWarm, warmValue);  // Ajuster l'intensité des LED blanc chaud
+  analogWrite(pinCool, coolValue);  // Ajuster l'intensité des LED blanc froid
 }
 
 void loop() {
-
+  if(hasChanged) {
+    if (millis() - time > 10000) {
+      // Serial.println("save to EEprom");
+      EEPROM.write(WARM_ADDRESS, warmValue);
+      EEPROM.write(COOL_ADDRESS, coolValue);
+      hasChanged = false;
+    }
+  }
   if (IrReceiver.decode()) {
     uint16_t command = IrReceiver.decodedIRData.command;
     Serial.println(command);
@@ -42,49 +54,55 @@ void loop() {
     potValue = command;
     power = coolValue + warmValue;
     ratio = coolValue / power;
-      
+    time = millis();
     switch (potValue) {
       case DECREASE_K:
         coolValue++;
         warmValue = power - coolValue;
+        hasChanged = true;
         break;
       case INCREASE_K:
         coolValue--;
         warmValue = power - coolValue;
+        hasChanged = true;
         break;
       case DECREASE_P:
-        if (coolValue > 0 && warmValue > 0) {
         coolValue--;
         warmValue--;
-        }
+        hasChanged = true;
         break;
       case INCREASE_P:
-        if (coolValue < 255 && warmValue < 255) {
-          coolValue++;
-          warmValue++;
-        }
+        coolValue++;
+        warmValue++;
+        hasChanged = true;
         break;
       case FULL_WHITE:
         coolValue=power;
         warmValue=0;
+        hasChanged = true;
         break;
       case MEDIUM_WHITE:
         coolValue=3*(power)/4;
         warmValue=power/4;
+        hasChanged = true;
         break;
       case MEDIUM_YELLOW:
         coolValue=power/2;
         warmValue=power/2;
+        hasChanged = true;
         break;
       case FULL_YELLOW:
         coolValue=0;
         warmValue=power;
+        hasChanged = true;
         break;
       case OFF:
         coolValue=0;
+        warmValue=0;
         break;
       case ON:
-        coolValue=0;
+        coolValue=EEPROM.read(COOL_ADDRESS);
+        warmValue=EEPROM.read(WARM_ADDRESS);
         break;
     }
     
@@ -96,6 +114,6 @@ void loop() {
   analogWrite(pinWarm, warmValue);  // Ajuster l'intensité des LED blanc chaud
   analogWrite(pinCool, coolValue);  // Ajuster l'intensité des LED blanc froid
 
-  Serial.print(warmValue);Serial.print(",");
-  Serial.println(coolValue);
+   Serial.print(warmValue);Serial.print(",");
+   Serial.println(coolValue);
 }
